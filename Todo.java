e33bb3146989c08b7e32dc3707072da676facd00
jthae <40273807+jthae@users.noreply.github.com>
2019-02-26 22:32:34 -0600

    Commit todo

---
+++ Todo.java
@@ -0,0 +1,48 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-19
+OU ID_______________: 113397751
+OU EMAIL____________: jayciejthaemert@ou.edu
+
+Tasks:
+1. complete circle class
+2. complete driver
+3. complete ellipse
+4. complete equilateraltriangle
+5. complete polygon
+6. implement rectangle
+7. implement square
+8. complete area comparator
+9. complete perimeter comparator
+10. complete shape sorter
+11. complete trapezoid
+12. complete test classes
+
+Estimated Time (minutes):
+1. 10
+2. 10
+3. 10
+4. 10
+5. 10
+6. 5
+7. 5
+8. 15
+9. 10
+10. 15
+11. 10
+12. 20
+
+Actual Time (minutes):
+1. 15
+2. 5
+3. 20
+4. 10
+5. 15
+6. 10
+7. 10
+8. 30
+9. 30
+10. 3
+11. 10
+12. 30
+*/}
